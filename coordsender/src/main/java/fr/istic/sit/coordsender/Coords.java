fde11a93d31b63d3ea703532d02ca760192901b2
JulesC <jules.chapron@gmail.com>
2015-02-12 09:27:15 +0100

    Initialize coords sender service

---
+++ coordsender/src/main/java/fr/istic/sit/coordsender/Coords.java
@@ -0,0 +1,19 @@
+package fr.istic.sit.coordsender;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by jules on 12/02/15.
+ */
+public class Coords implements Parcelable {
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
