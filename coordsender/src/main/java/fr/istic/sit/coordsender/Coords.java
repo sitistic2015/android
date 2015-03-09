c51d2041398f7f781a95914198391029adb9af40
JulesC <jules.chapron@gmail.com>
2015-03-09 08:24:41 +0100

    Parcelable first version

--- coordsender/src/main/java/fr/istic/sit/coordsender/Coords.java
+++ coordsender/src/main/java/fr/istic/sit/coordsender/Coords.java
@@ -2,11 +2,21 @@ package fr.istic.sit.coordsender;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Pair;
+
+import java.util.List;
 
 /**
  * Created by jules on 12/02/15.
  */
 public class Coords implements Parcelable {
+
+    private List<Pair<Double, Double>> coords;
+
+    public Coords(List<Pair<Double, Double>> coords) {
+        this.coords = coords;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -14,6 +24,11 @@ public class Coords implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-
+        double _coords [][]= new double[coords.size()][2];
+        for(int i = 0; i < coords.size(); i++) {
+            _coords[i][0] = coords.get(i).first;
+            _coords[i][1] = coords.get(i).second;
+        }
+        
     }
 }
