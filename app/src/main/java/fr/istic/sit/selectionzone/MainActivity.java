d555405ea2f3dd857c646fc2a9159134d0b9af06
Florian Etourneau <florian.etourneau@gmail.com>
2015-03-09 10:27:04 +0100

    Add : communication to the service

--- app/src/main/java/fr/istic/sit/selectionzone/MainActivity.java
+++ app/src/main/java/fr/istic/sit/selectionzone/MainActivity.java
@@ -1,13 +1,20 @@
 package fr.istic.sit.selectionzone;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -15,6 +22,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mapquest.android.maps.DefaultItemizedOverlay;
 import com.mapquest.android.maps.GeoPoint;
@@ -28,6 +36,9 @@ import com.mapquest.android.maps.PolygonOverlay;
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.istic.sit.coordsender.Coordinates;
+import fr.istic.sit.coordsender.CoordsSenderService;
+
 
 public class MainActivity extends MapActivity {
 
@@ -36,12 +47,29 @@ public class MainActivity extends MapActivity {
     protected ScrollView scroller;
     protected List<GeoPoint> polyData = new ArrayList();
 
+    /** Messenger for communicating with service. */
+    Messenger mService = null;
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mIsBound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
         handleClickOnMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        doBindService();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        doUnbindService();
     }
 
     protected void init() {
@@ -115,16 +143,33 @@ public class MainActivity extends MapActivity {
     }
 
     public void validateCoords(View view){
-        List<Coord> coords = new ArrayList();
+        List<Pair<Double,Double>> coords = new ArrayList();
 
-        for (GeoPoint point : polyData){
-            coords.add(new Coord(point.getLatitude(),point.getLongitude()));
+        for (GeoPoint point : polyData) {
+            coords.add(new Pair<Double, Double>(point.getLatitude(), point.getLongitude()));
         }
 
-        for (Coord coord : coords){
-            addEventText("Point : "+coord.getLatitude()+","+coord.getLongitude());
+        for (Pair<Double, Double> coord : coords){
+            addEventText("Point : "+coord.first+","+coord.second);
         }
 
+
+        Coordinates coordinates = new Coordinates(coords);
+
+        try {
+            Message msg = Message.obtain(null,
+                    CoordsSenderService.MSG_ZONE);
+            msg.replyTo = mMessenger;
+            Bundle bundle = new Bundle();
+            bundle.putParcelable("coord",coordinates);
+            msg.setData(bundle);
+            mService.send(msg);
+        } catch (RemoteException e) {
+            // In this case the service has crashed before we could even
+            // do anything with it; we can count on soon being
+            // disconnected (and then reconnected if it can be restarted)
+            // so there is no need to do anything here.
+        }
     }
 
     private void addEventText(String text){
@@ -171,4 +216,96 @@ public class MainActivity extends MapActivity {
                 Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
+
+
+
+    /**
+     * Handler of incoming messages from service.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case CoordsSenderService.MSG_ZONE:
+                    addEventText("Received from service: " + msg.arg1);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+
+
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  We are communicating with our
+            // service through an IDL interface, so get a client-side
+            // representation of that from the raw service object.
+            mService = new Messenger(service);
+            addEventText("Attached.");
+
+
+
+            // As part of the sample, tell the user what happened.
+            Toast.makeText(MainActivity.this, "Service connected",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            addEventText("Disconnected.");
+
+            // As part of the sample, tell the user what happened.
+            Toast.makeText(MainActivity.this, "Service disconnected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    void doBindService() {
+        // Establish a connection with the service.  We use an explicit
+        // class name because there is no reason to be able to let other
+        // applications replace our component.
+        bindService(new Intent(MainActivity.this,
+                CoordsSenderService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        addEventText("Binding.");
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with
+            // it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null,
+                            CoordsSenderService.MSG_ZONE);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service
+                    // has crashed.
+                }
+            }
+
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+            addEventText("Unbinding.");
+        }
+    }
+
 }
