dddfd14303848552910628ae9983a6c392deda3d
Corentin CLEMENT <corentin.clement57@gmail.com>
2015-03-10 17:18:13 +0100

    Create AbstractDAO

---
+++ server/src/main/java/dao/AbstractDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import util.Constant;
+
+/**
+ * Abstract class for DAO
+ * AbstractDAO provides methods for DAO
+ * Also use for connect and disconnect
+ */
+public abstract class AbstractDAO<Entity> {
+    protected static Cluster currentCluster;
+
+    private AbstractDAO() {}
+
+    protected static Bucket connect() {
+        // Connect to a cluster
+        currentCluster = CouchbaseCluster.create(Constant.COUCHBASE_HOSTNAME);
+
+        // Open the default bucket
+        Bucket bucket = currentCluster.openBucket(Constant.BUCKET_NAME);
+        return bucket;
+    }
+
+    protected static void disconnect() {
+        // Disconnect from the cluster
+        currentCluster.disconnect();
+    }
+}
---
+++ server/src/main/java/util/Constant.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * Created by corentin on 10/03/15.
+ */
+public class Constant {
+    public static final String COUCHBASE_HOSTNAME = "5.135.185.191";
+    public static final String BUCKET_NAME = "default";
+}
