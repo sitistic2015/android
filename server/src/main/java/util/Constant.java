175647d01980535a00036c587bda0bfa4544d757
Corentin CLEMENT <corentin.clement57@gmail.com>
2015-03-11 11:00:46 +0100

    server : update COUCHBASE_HOSTNAME

--- server/src/main/java/util/Constant.java
+++ server/src/main/java/util/Constant.java
@@ -4,6 +4,6 @@ package util;
  * Created by corentin on 10/03/15.
  */
 public class Constant {
-    public static final String COUCHBASE_HOSTNAME = "5.135.185.191";
+    public static final String COUCHBASE_HOSTNAME = "37.59.58.42";
     public static final String BUCKET_NAME = "default";
 }
